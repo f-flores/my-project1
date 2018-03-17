3e540bfd31ac533a1d0fbbcc6a55b827b0beadd8
Fabian Flores <ffflores1@outlook.com>
2018-03-17 17:43:25 -0400

    Added card styling to project.

--- assets/js/my-financial.js
+++ assets/js/my-financial.js
@@ -1,41 +1,62 @@
 /* my-financial.js */
 const AlphaAPIKEY = "51J2DL5ZC7RQBE6J";
-const AlphaEndPoint = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=";
+// const AlphaEndPoint = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=";
 const AlphaBatch = "https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols=";
 const AlphaBatchSuffix = "&apikey=" + AlphaAPIKEY;
-const AlphaTS = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=";
-const AlphaTSSuffix= AlphaBatchSuffix;
-
-var alphaQueryURL;
+// const AlphaTS = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=";
+// const AlphaTSSuffix = AlphaBatchSuffix;
 
 // https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&outputsize=full&apikey=demo
 // https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=EPD&apikey=51J2DL5ZC7RQBE6J
 
 // renders stock info
+// <!--               <div class="card-body">
+// <h5 class="card-title">Special title treatment</h5>
+// <p class="card-text" id="financial-text">With supporting text below as a natural lead-in to additional content.</p>
+// </div>
+//
+// --------------------------------------------------------------------------
+// renderStockInfo uses the bootstrap 4 'card' functionality to render
+// the stocks information
+//
 function renderStockInfo(data) {
-  var stockDiv = $("<div>");
-  var htmlText;
+  var stockDiv = $("<div>"),
+      cardh5 = $("<h5>").addClass("card-title"),
+      cardText = $("<p>").addClass("card-text ticker-paragraph"),
+      htmlText;
 
-  htmlText = "<p>Symbol: " + data["1. symbol"] + "</p>";
-  htmlText += "<p>Price: " + data["2. price"] + "</p>";
-  htmlText += "<p>Volume: " + data["3. volume"] + "</p>";
+  // empty out any prior content of #stock-ticker-content
+  $("#stock-ticker-content").empty();
 
-  stockDiv.append(htmlText);
-  $("#financial-text").append(stockDiv);
+  stockDiv.addClass("card-body");
+  cardh5.text(data["1. symbol"]);
+  cardh5.attr("stock-sym", data["1. symbol"]);
+  htmlText = "Price: " + numeral(data["2. price"]).format("$0,0.00") + "<br />";
+  htmlText += "Volume: " + data["3. volume"];
+  console.log("price: " + numeral(data["2. price"]).format("$0,0.00"));
+
+  cardText.html(htmlText);
+
+  stockDiv.append(cardh5);
+  stockDiv.append(cardText);
+  $("#stock-ticker-content").append(stockDiv);
 }
 
 // renders financial text
-function renderFinancialText(data) {
-  var finDiv = $("<div>");
-  var htmlText;
+// function renderFinancialText(data) {
+//  var finDiv = $("<div>");
+//  var htmlText;
 
-  htmlText = "<p>Information: " + data["1. Information"] + "</p>";
-  htmlText += "<p>Symbol: " + data["2. Symbol"] + "</p>";
+//  htmlText = "<p>Information: " + data["1. Information"] + "</p>";
+//  htmlText += "<p>Symbol: " + data["2. Symbol"] + "</p>";
 
-  finDiv.append(htmlText);
-  $("#financial-text").append(finDiv);
-}
+//  finDiv.append(htmlText);
+//  $("#financial-text").append(finDiv);
+// }
 
+// --------------------------------------------------------------------------
+// buildBatchURL calls the alpha batch function with only one stock symbol
+//
 function buildBatchURL(sym) {
   var result,
       queryURL;
@@ -47,69 +68,47 @@ function buildBatchURL(sym) {
   $.ajax({
     "method": "GET",
     "url": queryURL
-  }).then((response) => {
-    // console.log(response);
+  }).
+  done((response) => {
     result = response;
     console.log(result);
     console.log(result["Stock Quotes"][0]);
     renderStockInfo(result["Stock Quotes"][0]);
+  }).
+  fail(() => {
+    console.log("Failure from alpha batch function");
   });
 }
 
-//---------------------------------------------------------------------
+// ---------------------------------------------------------------------
 // builds watchlist
 //
-function buildWlist(sym) {
-  var result,
-      queryURL;
+// function buildWatchlist(sym) {
+//  var result,
 
-  console.log("in buildWlist()");
+//      queryURL;
+
+//  console.log("in buildWatchlist()");
   // get time-last-refreshed
-  queryURL = AlphaTS + sym + AlphaTSSuffix;
-  console.log("batch url: " + queryURL);
+//  queryURL = AlphaTS + sym + AlphaTSSuffix;
+//  console.log("batch url: " + queryURL);
 
-  $.ajax({
-    "method": "GET",
-    "url": queryURL
-  }).then((response) => {
-    // console.log(response);
-    result = response;
-    console.log(result);
-    console.log(result["Stock Quotes"][0]);
-    renderStockInfo(result["Stock Quotes"][0]);
-  });
-}
+//  $.ajax({
+//    "method": "GET",
+//    "url": queryURL
+//  }).then((response) => {
+//    // console.log(response);
+//    result = response;
+//    console.log(result);
+//    console.log(result["Stock Quotes"][0]);
+//    renderStockInfo(result["Stock Quotes"][0]);
+//  });
+// }
 
 
-function buildQueryURL() {
-  var result;
-
-  alphaQueryURL = AlphaEndPoint + AlphaAPIKEY;
-
-  $.ajax({
-    "method": "GET",
-    "url": alphaQueryURL
-  }).then((response) => {
-    // console.log(response);
-    result = response;
-    console.log(result);
-    console.log(result['Meta Data']);
-    renderFinancialText(result['Meta Data']);
-  });
-}
-
-// .on("click") function associated with the find-stock button
-$("#find-stock").on("click", (event) => {
-  // build the query URL for the ajax request to the Alpha API
-  var queryURL = buildQueryURL();
-
-  event.preventDefault();
-
-  // empty the region associated with the articles
-  $("#financial-text").empty();
-
-});
-
+// -----------------------------------------------------------------------
+// implements "stock-ticker" function
+//
 $("#stock-ticker").on("click", (event) => {
   var stockSymbol = $("#stock-input").val().
                                       trim();
@@ -117,19 +116,22 @@ $("#stock-ticker").on("click", (event) => {
   console.log("in stock-ticker() ");
   $("#financial-text").empty();
 
+  // clear stock-input field
+  $("#stock-input").val("");
+
   event.preventDefault();
   buildBatchURL(stockSymbol);
 
 });
 
 $("#wlist-button").on("click", (event) => {
-  var stockSymbol = $("#wlist-input").val().
-                                      trim();
+ // var stockSymbol = $("#wlist-input").val().
+ //                                     trim();
 
   console.log("in wlist-button() ");
   $("#financial-text").empty();
 
   event.preventDefault();
-  // buildWlist(stockSymbol);
+  // buildWatchlist(stockSymbol);
 
 });
